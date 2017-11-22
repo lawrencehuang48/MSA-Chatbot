2e2348800a19e41a1e363c55cbf9a9ee57a74883
Lawrence Huang <lawrencehuang48@gmail.com>
2017-11-22 14:39:46 +1300

    Integrated LUIS

---
+++ controller/LuisDialog.js
@@ -0,0 +1,111 @@
+var builder = require('botbuilder');
+
+
+exports.startDialog = function (bot) {
+
+    var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/bce87b49-4868-4b85-b575-2ddcc0c345f5?subscription-key=1ba63520afe24644bc75129d6f89e19c&verbose=true&timezoneOffset=0&q=');
+
+    bot.recognizer(recognizer);
+
+    bot.dialog('WantFood', function (session, args) {
+        if (!isAttachment(session)) {
+            // Pulls out the food entity from the session if it exists
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'food');
+
+            // Checks if the food entity was found
+            if (foodEntity) {
+                session.send('Looking for restaurants which sell %s...', foodEntity.entity);
+                // Insert logic here later
+            } else {
+                session.send("No food identified! Please try again");
+            }
+        }
+
+    }).triggerAction({
+        matches: 'WantFood'
+    });
+
+    bot.dialog('DeleteFavourite', function (session, args) { 
+        // Insert delete logic here later
+        if (!isAttachment(session)) {
+            // Pulls out the food entity from the session if it exists
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'food');
+
+            // Checks if the food entity was found
+            if (foodEntity) {
+                session.send('Deleting %s...', foodEntity.entity);
+                // Insert logic here later
+            } else {
+                session.send("No food identified! Please try again");
+            }
+        }
+    }).triggerAction({
+        matches: 'DeleteFavourite'
+
+    });
+
+    bot.dialog('GetCalories', function (session, args) {
+        if (!isAttachment(session)) {
+
+            // Pulls out the food entity from the session if it exists
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'food');
+
+            // Checks if the for entity was found
+            if (foodEntity) {
+                session.send('Calculating calories in %s...', foodEntity.entity);
+                // Insert logic here later
+
+            } else {
+                session.send("No food identified! Please try again");
+            }
+        }
+    }).triggerAction({
+        matches: 'GetCalories'
+    });
+
+    bot.dialog('GetFavouriteFood', function (session,args) {
+       // Insert favourite food logic here later
+       if (!isAttachment(session)) {
+        
+            // Pulls out the food entity from the session if it exists
+            var foodEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'food');
+        
+            // Checks if the for entity was found
+            if (foodEntity) {
+                session.send('Retrieving favourite %s...', foodEntity.entity);
+                // Insert logic here later
+        
+            } else {
+                session.send("No food identified! Please try again");
+            }
+        }
+    }).triggerAction({
+        matches: 'GetFavouriteFood'
+    });
+
+    bot.dialog('LookForFavourite', [
+        // Insert logic here later
+    ]).triggerAction({
+        matches: 'LookForFavourite'
+    });
+    
+
+    bot.dialog('WelcomeIntent', [
+        // Insert logic here later
+    ]).triggerAction({
+        matches: 'WelcomeIntent'
+    });
+}
+
+// Function is called when the user inputs an attachment
+function isAttachment(session) { 
+    var msg = session.message.text;
+    if ((session.message.attachments && session.message.attachments.length > 0) || msg.includes("http")) {
+        
+        //call custom vision here later
+        return true;
+    }
+    else {
+        return false;
+    }
+}
