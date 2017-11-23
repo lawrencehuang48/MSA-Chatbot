364a24b05ca185b2e61d5824ece6f93a42c205c8
Lawrence Huang <lawrencehuang48@gmail.com>
2017-11-23 15:58:39 +1300

    Changed the app ID and password in app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "a430ac5a-a7a3-4947-98f3-4288e025f985",
+    appPassword: "rmqT67%*mtodPDBOYH715{~"
 });
 
 // Listen for messages from users 
